183614b00a58ad8d9024ee7c7bc7b32920bb42f8
prince <pk20287022@gmail.com>
2023-10-02 19:58:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from 'react'
-import  { useState } from 'react'
-import './App.css'
+import Header from './component/Header'
 
-export const App = () => {
-  const[pk,setPk]=useState(0)
-  const inc=()=>{
-    setPk(pk+1)
-    
-  }
-  const dec=()=>{
-    setPk(pk-1)
-  }
-  
-  
- 
+
+export default function App() {
   return (
-    <>
-    <div className='nav'>
-    <h1 className='count'>Counter App:{pk}</h1>
-    </div>
-    <div className='btn-1'>
-    <button className='btn'onClick={inc}>Add Value</button>
-    <button className='btn'onClick={dec}>less Vlaue</button>
-    </div>
-    
-    </>
+    <Header></Header>
   )
-  }
-export default App;
+}
